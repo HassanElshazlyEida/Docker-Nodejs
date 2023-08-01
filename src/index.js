91302f09dfad9116cdd2bd8de3bf2711b0a39f5e
HassanElshazlyEida <hassanaboeata@gmail.com>
2023-08-01 06:29:26 +0300

    💥 NEW: Connect to mysql

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
-
+var mysql = require('mysql');
 
 const PORT = 3000;
 const app = express();
+
+const DB_USER = "root";
+const DB_PASSWORD= "root";
+const DB_PORT='8889';
+const DB_HOST="localhost";
+
+var con = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password:DB_PASSWORD,
+    port:DB_PORT
+});
+
+con.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as is ' + con.threadId);
+  });
 
 app.listen(PORT, () => console.log("Running .. "));
 
